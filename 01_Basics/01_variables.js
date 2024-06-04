f13306949d4c69e64e6820e083da559cb863ce53
Om Mahajan <159154622+omahajan91@users.noreply.github.com>
2024-06-04 11:24:18 +0000

    01_variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,22 @@
+console.log("you are the only one who can do it")
+
+const account_Id = 443201
+let Email = "ormahajan@gmail.com"
+var password = "i_am_going"
+
+let accountState;
+
+// accountId = 2 // not allowed
+
+Email = "newemail@com"
+password = "2121"
+
+
+// console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([account_Id, Email, password, accountState, ])
